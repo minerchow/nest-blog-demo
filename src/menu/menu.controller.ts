b66d82468fabb0d5c3dfba130c87a982e2482e59
zhouming <minerchow@126.com>
2025-02-01 21:12:11 +0800

    从token获取id

--- src/menu/menu.controller.ts
+++ src/menu/menu.controller.ts
@@ -1,15 +1,33 @@
 // src/menu/menu.controller.ts
-import {  Controller, Post, Put, Delete, Body, Param , Get ,Query } from '@nestjs/common';
+import {  Controller, Post, Put, Delete, Body, Param , Get ,Query, UseGuards, Req } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { MenuEntity } from './entities/menu.entity';
-
+import { JwtAuthGuard } from './../auth/jwt-auth.guard';
+import { RolesGuard, Roles } from './../auth/role.guard';
+import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 @Controller('menu')
-export class MenuController {
-  constructor(private readonly menuService: MenuService) {}
+export class MenuController extends AuthGuard('jwt') {
+  constructor(
+    private readonly menuService: MenuService,
+    private readonly jwtService: JwtService,
+  ) {
+    super();
+  }
 
+  @Roles('root')
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('list')
-  async getMenuList(@Query('userId') userId: string): Promise<MenuEntity[]> {
-    return this.menuService.getMenuList(userId);
+  async getMenuList(@Req() req: Request,@Query('userId') userId: string): Promise<MenuEntity[]> {
+    
+    const token = req.headers.authorization?.replace('Bearer ', '');
+    
+    if(!token){
+      return [];
+    }
+    const decoded = this.jwtService.decode(token) as {id:number}
+    return this.menuService.getMenuList(decoded.id.toString());
   }
 
   // 根据菜单 id 获取菜单信息的路由
--- src/menu/menu.module.ts
+++ src/menu/menu.module.ts
@@ -4,9 +4,13 @@ import { MenuController } from './menu.controller';
 import { MenuEntity } from './entities/menu.entity';
 import { User } from './../user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([User, MenuEntity])],
+  imports: [TypeOrmModule.forFeature([User, MenuEntity]), 
+    AuthModule,
+    UserModule],
   controllers: [MenuController],
   providers: [MenuService]
 })
-export class MenuModule {}
+export class MenuModule { }
